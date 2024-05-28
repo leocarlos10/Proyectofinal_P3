a53caf0b2714c4bc0522fe3acbe9742b90ef0c54
Calebsenm <calebgamercraft@gmail.com>
2024-05-28 16:34:42 -0500

    Connectar bd

---
+++ ProyectoP3/src/ConexionDAO/ConexionMySQL.java
@@ -0,0 +1,42 @@
+package ConexionDAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConexionMySQL {
+
+    private static Connection con;
+
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String user = "root";
+    private static final String paswword = "password";
+    private static final String url = "jdbc:mysql://localhost/DBlorica";
+
+    public ConexionMySQL() {
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, paswword);
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
+
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+
+    public void desconectar() {
+        con = null;
+    }
+
+    public void cerrar() throws SQLException {
+        if (con != null) {
+            con.close();
+        }
+    }
+
+}
---
+++ ProyectoP3/src/ConexionDAO/ProductoBD.java
@@ -0,0 +1,40 @@
+package ConexionDAO;
+
+public class ProductoBD {
+
+}
+
+/*
+
+Producto: 
+    String nombre,
+    String tipo_E_sistema,
+    String descripcion,
+    String coleccion,
+    String categoria,
+    int precio
+
+Pedido: 
+    String nombre,
+    String tipo_E_sistema,
+    int C_unidades,
+    int precio,
+    LocalDate fecha
+
+Cliente:
+    String id;
+    String nombre;
+    String direccion;
+    String tipo_entidad;
+    String documento;
+    String tipo_producto;
+    int precio_producto;
+
+Proveedor 
+    String nombre,
+    String direccion,
+    String tipo_entidad,
+    String documento,
+    String tipo_producto
+
+ */
